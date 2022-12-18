4a0a1f6debdb80bc9da2c12c785cbac9a9eedb01
smcdev <smcdev@outlook.com>
2022-12-17 18:44:17 -0600

    Update app.js

--- 2_5_project_files/js_sandbox/app.js
+++ 2_5_project_files/js_sandbox/app.js
@@ -1,29 +1,58 @@
 let val;
 
 // Number to string
+val = 5;
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+val = String(5);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
 val = String(555);
-val = String(4+4);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+val = String(4 + 4);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
 // Bool to string
 val = String(true);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
 // Date to string
 val = String(new Date());
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
 // Array to string
-val = String([1,2,3,4]);
+val = String([1, 2, 3, 4]);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
 // toString()
 val = (5).toString();
-val = (true).toString();
+val = true.toString();
 
 // String to number
-val = Number('5');
+val = Number("5");
 val = Number(true);
 val = Number(false);
 val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
+val = Number("hello");
+val = Number([1, 2, 3]);
 
-val = parseInt('100.30');
-val = parseFloat('100.30');
+val = parseInt("100.30");
+val = parseFloat("100.30");
 
 // Output
 // console.log(val);
